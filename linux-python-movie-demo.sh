47e73f14522345233fda418e033e4a886fa7f1d8
Bill Wilder <codingoutloud@gmail.com>
2018-04-21 14:46:02 +0530

    Update linux-python-movie-demo.sh

--- linux-python-movie-demo.sh
+++ linux-python-movie-demo.sh
@@ -6,3 +6,5 @@ az group list
 az group create --name kolazsa --location southeastasia
 az container create --resource-group kolazsa --name movie-smasher-green --image kolaz.azurecr.io/movie-smasher:latest --restart-policy OnFailure --environment-variables CONTAINER_NAME=saffron --registry-password 'oTnE76kiK6qBCk2TH+uo=ezNHrRKYOmr'
 az container show --resource-group kolazsa --name movie-smasher-green --query "{FQDN:ipAddress.fqdn,ProvisioningState:provisioningState}" --out table
+
+# end
